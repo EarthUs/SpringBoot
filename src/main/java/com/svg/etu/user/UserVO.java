2efd2e0220a154a79d7c5a1211c6725336e835dc
HyeminNoh <shrmffkfk@naver.com>
2020-06-12 14:46:28 +0900

    role 삭제

--- src/main/java/com/svg/etu/user/UserVO.java
+++ src/main/java/com/svg/etu/user/UserVO.java
@@ -26,14 +26,11 @@ public class UserVO {
 		this.img = img;
 		this.nick_name = nick_name;
 		this.email = email;
-		this.role = role;
 		this.steps = steps;
 		this.location = location;
 	}
 
-	public String getRoleKey() {
-		return this.role.getKey();
-	}
+
 }
 
 // 소셜 로그인으로 반환되는 값 중 email을 통해 이미 생성된 사용자인지 처음 가입하는 사용자인지 판단하기 위한 메소드
